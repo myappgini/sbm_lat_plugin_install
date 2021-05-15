763ccb5207da155037bea2f46bf8fe023fc1a473
Alejandro <landinialejandro@hotmail.com>
2021-05-14 23:50:15 -0300

    fix files location

--- lat_class.php
+++ lat_class.php
@@ -1,5 +1,5 @@
 <?php
-	include(dirname(__FILE__).'loader.php');
+	include(dirname(__FILE__).'/../plugins-resources/loader.php');
 
 	class lat_class extends AppGiniPlugin{
 		/* add any plugin-specific properties here */
